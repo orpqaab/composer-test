de2e13e34f1fb23d5b42eb2b95cabdf565730a68
orpqaab <orpqaab@gmail.com>
2017-01-05 20:24:22 +0100

    deleted helper

--- controllers/CreateController.php
+++ controllers/CreateController.php
@@ -4,16 +4,12 @@ namespace Work\ComposerTest\Controllers;
 
 class CreateController {
 
-    private $employeeRepository;
-
-    function __construct() {
-        $this->employeeRepository = new \Bevprog\CrudDemo\Util\EmployeeRepository();
-    }
-
     private function getOutput() {
         ob_start();
         include \Work\ComposerTest\Configuration::VIEWS_DIR . "/create_success.html";
         return ob_get_flush();
     }
-
+    private function showForm(){
+        
+    }
 }
